b8fd624e2e54f01347d3ad79e987c19212a5ca34
Rodrigo L. Oliveira <36026378+RodrigoLOliveira@users.noreply.github.com>
2023-10-14 02:28:25 -0300

    Parte 1

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<RandomService>();
+builder.Services.AddScoped<RandomService>();
 builder.Services.AddDbContext<TestDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("ctx")));
 var app = builder.Build();
